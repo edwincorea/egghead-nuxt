3fd46625f13a3d01926b8cc53975b6f7a3e547aa
Edwin Corea <Edwin Corea>
2017-04-15 09:45:28 -0600

    Add arrays of data to the Vuex store and display them in Vue.js template.

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="pa4">
-    {{counter}}
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+  <div>
+    <!--http://tachyons.io/components/lists/border-spaced/index.html-->
+    <article class="pa3 pa5-ns">
+      <h1 class="f4 bold center mw6">Todos</h1>
+      <ul class="list pl0 ml0 center mw6 ba b--light-silver br2">
+        <li v-for="todo of todos" class="ph3 pv3 bb b--light-silver">
+          {{todo.task}}
+        </li>
+      </ul>
+    </article>    
   </div>
 </template>
 
 <script>
   // https://github.com/vuejs/vuex
-  import {mapState, mapMutations} from 'vuex'
+  import {mapState} from 'vuex'
   
   export default {
     computed: {
       ...mapState({
-        counter: state => state.counter
+        todos: state => state.todos
       })
     },
 
     methods: {
-      ...mapMutations([
-        'increment',
-        'decrement'
-      ])
+
     }
   }
 </script>
